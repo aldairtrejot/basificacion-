176f3aeee1789cb765dc1b5ea417b9e09c2a51a9
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2024-06-20 09:53:53 -0600

    Cabio en conexion
    Primer paso de conexion para produccion

--- test/nav-bar1.php
+++ test/nav-bar1.php
@@ -4,5 +4,5 @@ DROP TABLE IF EXISTS asistencia_bas;
 CREATE TABLE IF NOT EXISTS asistencia_bas(
 	id_asistencia_bas SERIAL PRIMARY KEY,
 	fecha TIMESTAMP,
-	id_id_tbl_empleados_hraes INTEGER
+	id_tbl_empleados_hraes INTEGER
 )
