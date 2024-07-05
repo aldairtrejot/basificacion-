025d751d2172bc0919bc6b3af9957c5fd4f65ab0
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2024-07-05 14:55:30 -0600

    CAMBIOS

--- App/View/Hraes/Modulo/index.php
+++ App/View/Hraes/Modulo/index.php
@@ -100,12 +100,12 @@ if ($id_tbl_empleados_hraes == null) {
                                 onclick="iniciarEscolaridad();" role="tab" aria-controls="nav-contact"
                                 aria-selected="false"><i class="fa fa-graduation-cap"></i> Escolaridad</a>
 
-                            <!--
+                            
                                 <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-movimientos"
                                 onclick="iniciarMovimiento();" role="tab" aria-controls="nav-contact"
                                 aria-selected="false"><i class="fa fa-random"></i> Movimientos</a>
 
-
+<!--
                             
                                                             <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-programas"
                                 onclick="iniciarProgramas();" role="tab" aria-controls="nav-contact"
